f2aad11a280db6f87c5cba8a5edc6b39dc9f272a
Wolfgang Faust <wolfgang42@git>
2017-09-10 16:34:48 -0400

    Clean up .new files if they haven't changed.

--- update.sh
+++ update.sh
@@ -11,7 +11,9 @@ curl -s https://releases.hashicorp.com/index.json > cache/releases.json
 for arch in i386 amd64; do
 	$BASEDIR/build-packages.py $arch > public_html/dists/any/main/binary-$arch/Packages.new
 	pushd public_html/dists/any/main/binary-$arch/ > /dev/null
-	if ! cmp -s Packages Packages.new; then
+	if cmp -s Packages Packages.new; then
+		rm Packages.new
+	else
 		echo "Updated Packages for $arch"
 		mv Packages.new Packages
 		gzip -kf Packages
@@ -33,7 +35,9 @@ echo 'Codename: any' >> Release.new
 echo 'Components: main' >> Release.new
 echo 'Architectures: i386 amd64' >> Release.new
 $BASEDIR/build-release-checksums.py main/binary-{amd64,i386}/{Release,Packages{,.gz,.bz2}} >> Release.new
-if ! cmp -s Release Release.new; then
+if cmp -s Release Release.new; then
+	rm Release.new
+else
 	echo "Updated Release"
 	mv Release.new Release
 	rm -f InRelease Release.gpg
